40871af99474124476df3a857e3bb7877b79df4a
Connor French <cbfrench@calpoly.edu>
2017-04-05 19:39:28 -0700

    Began adding dropdown menu

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+$(function(){
+    $("#projectsButton").click(function(){
+        $(".dropdown-content").toggle(500);
+        $(".dropdown-content").style.display = "block";
+    });
+});
+$(function(){
+    $("#aboutButton").click(function(){
+        $("body").fadeOut(500);
+        setTimeout(goToAbout, 500);
+        $(body).style.display = "none";
+    });
+});
+$(function(){
+    $("#homeButton").click(function(){
+        $("body").fadeOut(500);
+        setTimeout(goToHome, 500);
+        $(body).style.display = "none";
+    });
+});
 function goToAbout(){
     window.location.href = "aboutPage.html";
 }

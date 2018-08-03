a1d8813e5d1ee1b5b22ab8fc2678f30486523603
Connor French <connorfrench716@gmail.com>
2018-08-03 04:33:07 -0700

    Added system sorting and system labels

--- reviews/script.js
+++ reviews/script.js
@@ -1,11 +1,18 @@
+var term = "all systems";
+
 $(document).ready(function(){
     sortReviews();
+    sortSystem();
     $("#review-sort").change(function(){
         $(".review-link").fadeTo(200, 0).delay(200);
         setTimeout(sortReviews, 300);
         $(".review-link").fadeTo(200, 1);
     });
-
+    $("#review-system").change(function(){
+        $(".review-link").fadeTo(200, 0).delay(200);
+        setTimeout(sortSystem, 300);
+        $(".review-link").fadeTo(200, 1);
+    });
 
 function sortReviews(){
     var reviews = document.getElementsByClassName("review-link");
@@ -39,6 +46,42 @@ function sortReviews(){
     if(v == "lowest score"){
         for(var i = 0; i < reviews.length; i++){
             reviews[i].style.order = reviews[i].children[0].getAttribute("score");
+        }
+    }
+    if(term == "all systems"){
+        return;
+    }
+    for(var i = 0; i < reviews.length; i++){
+        if(term != reviews[i].children[0].getAttribute("system")){
+            reviews[i].style.display = "none";
+        }
+    }
+}
+
+function sortSystem(){
+    var reviews = document.getElementsByClassName("review-link");
+    var s = document.getElementById("review-system");
+    var v = s.options[s.selectedIndex].text;
+    v = v.toLowerCase();
+    if(v == "nintendo switch"){
+        term = "switch";
+    }
+    if(v == "nintendo wii u"){
+        term = "wii u";
+    }
+    if(v == "nintendo gamecube"){
+        term = "gamecube";
+    }
+    if(v == "nintendo 3DS"){
+        term = "3DS";
+    }
+    if(v == "all systems"){
+        term = "all systems";
+        return;
+    }
+    for(var i = 0; i < reviews.length; i++){
+        if(term != reviews[i].children[0].getAttribute("system")){
+            reviews[i].style.display = "none";
         }
     }
 }
